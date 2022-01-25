5f241898b8dca08255ff79d8fb67a8aee96525ec
Suyash Bahrani <suyash.bahrani@gmail.com>
2022-01-25 17:35:10 -0600

    ejs implemented for testing date i.e weekday/end

--- app.js
+++ app.js
@@ -6,7 +6,19 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
-    res.send("Hello");
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+    //0-6 sun-mon
+    //render for ejs (name of file)
+    //ejs have variable: value
+    if (currentDay === 6 || currentDay === 0) {
+        day = "Weekend";
+    }
+    else {
+        day = "weekday";
+    }
+    res.render("list", {kindOfDay: day})
 });
 
 
